dd868077bb8b5e4907e76c88a7fc7fa625e6b7d4
Ilan <Ilan>
2015-11-20 17:42:37 -0500

    scrapes multiple kijiji pages

--- app.js
+++ app.js
@@ -12,31 +12,39 @@ app.listen(process.env.PORT || 3000, function() {
 });
 
 app.get('/kjj', function(req, res) {
-    request({
-        uri: "http://www.kijiji.ca/b-ville-de-montreal/macbook/k0l1700281",
-    }, function(error, response, body) {
-        var $ = cheerio.load(body);
-        var obj = {};
-        $('tr').each(function(i, element) {
-            var $posted = $(element).find('.posted').text().trim();
-            // Verify is it has a posted date to prevent scraping ads
-            if ($posted.length) {
-                var $descContainer = $(element).find('.description'),
-                    $urlTxt = $descContainer.find('a').text().trim(),
-                    $urlRef = $descContainer.find('a').attr('href').trim(),
-                    $description = $descContainer.find('p').text().trim();
-                var $price = $(element).find('.price').text().trim();
-                var $image = $(element).find('.image').find('img').attr('src').trim();
-                obj[i] = {
-                    urlTxt: $urlTxt,
-                    urlRef: $urlRef,
-                    description: $description,
-                    price: $price,
-                    image: $image,
-                    date: $posted
+    var pageURL = '',
+        page = 0,
+        data = 0,
+        obj = {};
+    do {
+        request({
+            uri: "http://www.kijiji.ca/b-ville-de-montreal/macbook" + pageURL + "/k0l1700281",
+        }, function(error, response, body) {
+            var $ = cheerio.load(body);
+            $('tr').each(function(i, element) {
+                var $posted = $(element).find('.posted').text().trim();
+                // Verify if it has a posted date to prevent scraping ads
+                if ($posted.length) {
+                    var $descContainer = $(element).find('.description'),
+                        $urlTxt = $descContainer.find('a').text().trim(),
+                        $urlRef = $descContainer.find('a').attr('href').trim(),
+                        $description = $descContainer.find('p').text().trim();
+                    var $price = $(element).find('.price').text().trim();
+                    var $image = $(element).find('.image').find('img').attr('src').trim();
+                    obj[data++] = {
+                        urlTxt: $urlTxt,
+                        urlRef: $urlRef,
+                        description: $description,
+                        price: $price,
+                        image: $image,
+                        date: $posted
+                    }
                 }
-            }
+            })
+            console.log(obj)
+
         })
-        console.log(obj)
-    })
+        page++;
+        pageURL = "/page-" + page;
+    } while (page < 10)
 })
