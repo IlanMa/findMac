db96fbf8a9296fb0624ba81223b6c3475f5ddfd1
Ilan <Ilan>
2015-11-20 16:28:01 -0500

    scrapes the initial page of kijiji

--- app.js
+++ app.js
@@ -8,17 +8,35 @@ var app = express();
 app.use(express.static(__dirname + '/client'));
 
 app.listen(process.env.PORT || 3000, function() {
-	console.log("Running Server...")
+    console.log("Running Server...")
 });
 
-app.get('/kjj', function(req, res){
-	request({
-	  uri: "http://www.kijiji.ca/b-ville-de-montreal/macbook/k0l1700281",
-	}, function(error, response, body) {
-		var $ = cheerio.load(body);
-		$('td.description').each(function(i, element) {
-			console.log($(this).children().next().text());
-		})
-	})
+app.get('/kjj', function(req, res) {
+    request({
+        uri: "http://www.kijiji.ca/b-ville-de-montreal/macbook/k0l1700281",
+    }, function(error, response, body) {
+        var $ = cheerio.load(body);
+        var obj = {};
+        $('tr').each(function(i, element) {
+            var $posted = $(element).find('.posted').text().trim();
+            // Verify is it has a posted date to prevent scraping ads
+            if ($posted.length) {
+                var $descContainer = $(element).find('.description'),
+                    $urlTxt = $descContainer.find('a').text().trim(),
+                    $urlRef = $descContainer.find('a').attr('href').trim(),
+                    $description = $descContainer.find('p').text().trim();
+                var $price = $(element).find('.price').text().trim();
+                var $image = $(element).find('.image').find('img').attr('src').trim();
+                obj[i] = {
+                    urlTxt: $urlTxt,
+                    urlRef: $urlRef,
+                    description: $description,
+                    price: $price,
+                    image: $image,
+                    date: $posted
+                }
+            }
+        })
+        console.log(obj)
+    })
 })
-
